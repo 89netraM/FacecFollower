7229540c0dd6518bd8cbe02e1e32e3581829e45b
Mårten Åsberg <marten.asberg@outlook.com>
2020-05-16 14:47:14 +0200

    Read (and show) webcam

--- Application/FaceFollower/MainWindow.xaml.cs
+++ Application/FaceFollower/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +14,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using AForge.Video;
+using AForge.Video.DirectShow;
 
 namespace FaceFollower
 {
@@ -20,9 +24,44 @@ namespace FaceFollower
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private readonly VideoCaptureDevice videoSource;
+
 		public MainWindow()
 		{
 			InitializeComponent();
+
+			FilterInfoCollection videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+			videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
+			videoSource.NewFrame += VideoSource_NewFrame;
+			videoSource.Start();
+		}
+
+		private void VideoSource_NewFrame(object sender, NewFrameEventArgs args)
+		{
+			if (args.Frame.Clone() is Bitmap bitmap)
+			{
+				this.Dispatcher.Invoke(() =>
+				{
+					BitmapImage source = new BitmapImage();
+					using (Stream stream = new MemoryStream())
+					{
+						bitmap.Save(stream, ImageFormat.Png);
+						stream.Position = 0;
+						source.BeginInit();
+						source.CacheOption = BitmapCacheOption.OnLoad;
+						source.StreamSource = stream;
+						source.EndInit();
+					}
+					image.Source = source;
+				});
+			}
+		}
+
+		protected override void OnClosed(EventArgs e)
+		{
+			videoSource.SignalToStop();
+
+			base.OnClosed(e);
 		}
 	}
 }
