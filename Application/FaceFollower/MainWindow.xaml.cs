8ce9f8072597232bdec809fb77e0cf39cf04e25f
Mårten Åsberg <marten.asberg@outlook.com>
2020-05-17 14:51:02 +0200

    OpenCV face detection

--- Application/FaceFollower/MainWindow.xaml.cs
+++ Application/FaceFollower/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
@@ -17,10 +17,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Threading;
-using AForge.Video;
-using AForge.Video.DirectShow;
-using Microsoft.Azure.CognitiveServices.Vision.Face;
-using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+using Emgu.CV;
+using Emgu.CV.Structure;
 
 namespace FaceFollower
 {
@@ -29,42 +27,55 @@ namespace FaceFollower
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		private static readonly string azureKey = Environment.GetEnvironmentVariable("AZURE_FACE_KEY");
-		private static readonly string azureEndpoint = Environment.GetEnvironmentVariable("AZURE_FACE_ENDPOINT");
+		private readonly VideoCapture videoSource;
+		private readonly CascadeClassifier[] detectors;
+		private readonly Mat frame = new Mat();
 
-		private readonly VideoCaptureDevice videoSource;
-		private readonly IFaceClient faceClient;
-
-		private Bitmap latestImage = new Bitmap(1, 1);
 		private bool faceDetectionRunning = false;
 
 		public MainWindow()
 		{
 			InitializeComponent();
 
-			FilterInfoCollection videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
-			videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
-			videoSource.NewFrame += VideoSource_NewFrame;
-			videoSource.Start();
+			detectors = new[]
+			{
+				new CascadeClassifier(Path.GetFullPath(@".\Algo\haarcascade_frontalface_default.xml")),
+				new CascadeClassifier(Path.GetFullPath(@".\Algo\haarcascade_frontalface_alt.xml")),
+				new CascadeClassifier(Path.GetFullPath(@".\Algo\haarcascade_frontalface_alt2.xml")),
+				new CascadeClassifier(Path.GetFullPath(@".\Algo\haarcascade_profileface.xml"))
+			};
 
-			faceClient = new FaceClient(new ApiKeyServiceClientCredentials(azureKey), new DelegatingHandler[] { });
-			faceClient.Endpoint = azureEndpoint;
+			videoSource = new VideoCapture();
+			videoSource.ImageGrabbed += VideoSource_NewFrame;
+			videoSource.Start();
 		}
 
-		private void VideoSource_NewFrame(object sender, NewFrameEventArgs args)
+		private void VideoSource_NewFrame(object sender, EventArgs args)
 		{
-			if (args.Frame.Clone() is Bitmap bitmap)
+			if (videoSource.Retrieve(frame) && frame.ToImage<Bgr, byte>() is Image<Bgr, byte> imageFrame)
 			{
-				lock (latestImage)
+				if (faceDetectionRunning)
 				{
-					latestImage = bitmap;
+					Image<Gray, byte> grayFrame = imageFrame.Convert<Gray, byte>();
+					List<Rectangle> faces = new List<Rectangle>();
+					for (int i = 0; i < detectors.Length; i++)
+					{
+						faces.AddRange(detectors[i].DetectMultiScale(grayFrame, 1.3, 5));
+					}
+
+					foreach (Rectangle face in faces)
+					{
+						imageFrame.Draw(face, new Bgr(System.Drawing.Color.Red), 2);
+					}
 				}
+
 				this.Dispatcher.Invoke(() =>
 				{
 					BitmapImage source = new BitmapImage();
 					using (Stream stream = new MemoryStream())
 					{
-						bitmap.Save(stream, ImageFormat.Bmp);
+						byte[] imageData = imageFrame.ToJpegData();
+						stream.Write(imageData, 0, imageData.Length);
 						stream.Position = 0;
 						source.BeginInit();
 						source.CacheOption = BitmapCacheOption.OnLoad;
@@ -76,71 +87,17 @@ namespace FaceFollower
 			}
 		}
 
-		private void image_SizeChanged(object sender, SizeChangedEventArgs e)
-		{
-			canvas.Width = e.NewSize.Width;
-		}
-
 		private void StartStop_Click(object sender, RoutedEventArgs e)
 		{
 			if (faceDetectionRunning)
 			{
 				faceDetectionRunning = false;
-				faceRect.Visibility = Visibility.Hidden;
 				SetStatus("Stopped");
 			}
 			else
 			{
 				faceDetectionRunning = true;
-				Task.Run(Timer_Tick);
 				SetStatus("Running...");
-			}
-		}
-
-		private async void Timer_Tick()
-		{
-			while (faceDetectionRunning)
-			{
-				try
-				{
-					IList<DetectedFace> faces;
-					using (Stream stream = new MemoryStream())
-					{
-						lock (latestImage)
-						{
-							latestImage.Save(stream, ImageFormat.Bmp);
-						}
-						stream.Position = 0;
-						faces = await faceClient.Face.DetectWithStreamAsync(stream);
-					}
-
-					if (faceDetectionRunning)
-					{
-						this.Dispatcher.Invoke(() =>
-						{
-							if (faces.Count > 0 && faces[0] is DetectedFace face)
-							{
-								faceRect.Visibility = Visibility.Visible;
-								faceRect.Width = face.FaceRectangle.Width / (double)latestImage.Width * image.ActualWidth;
-								faceRect.Height = face.FaceRectangle.Height / (double)latestImage.Height * image.ActualHeight;
-								Canvas.SetLeft(faceRect, face.FaceRectangle.Left / (double)latestImage.Width * image.ActualWidth);
-								Canvas.SetTop(faceRect, face.FaceRectangle.Top / (double)latestImage.Height * image.ActualHeight);
-								SetStatus($"Running: Found face [{face.FaceRectangle.Left}, {face.FaceRectangle.Top}, {face.FaceRectangle.Width}, {face.FaceRectangle.Height}]");
-							}
-							else
-							{
-								SetStatus("Running: No face found");
-							}
-						});
-					}
-				}
-				// Catch and display Face API errors.
-				catch (APIErrorException ex)
-				{
-					MessageBox.Show(ex.Message, "API Error");
-				}
-
-				await Task.Delay(3000);
 			}
 		}
 
@@ -151,7 +108,7 @@ namespace FaceFollower
 
 		protected override void OnClosed(EventArgs e)
 		{
-			videoSource.SignalToStop();
+			videoSource.Stop();
 
 			base.OnClosed(e);
 		}
