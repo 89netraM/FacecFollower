328cf10b0e2d2772c1ae181023eac6b6021613c1
Mårten Åsberg <marten.asberg@outlook.com>
2020-05-17 10:34:54 +0200

    Azure face detection

--- Application/FaceFollower/MainWindow.xaml.cs
+++ Application/FaceFollower/MainWindow.xaml.cs
@@ -1,10 +1,12 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -14,8 +16,11 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
+using System.Windows.Threading;
 using AForge.Video;
 using AForge.Video.DirectShow;
+using Microsoft.Azure.CognitiveServices.Vision.Face;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
 
 namespace FaceFollower
 {
@@ -24,7 +29,14 @@ namespace FaceFollower
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private static readonly string azureKey = Environment.GetEnvironmentVariable("AZURE_FACE_KEY");
+		private static readonly string azureEndpoint = Environment.GetEnvironmentVariable("AZURE_FACE_ENDPOINT");
+
 		private readonly VideoCaptureDevice videoSource;
+		private readonly IFaceClient faceClient;
+
+		private Bitmap latestImage = new Bitmap(1, 1);
+		private bool faceDetectionRunning = false;
 
 		public MainWindow()
 		{
@@ -34,18 +46,25 @@ namespace FaceFollower
 			videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
 			videoSource.NewFrame += VideoSource_NewFrame;
 			videoSource.Start();
+
+			faceClient = new FaceClient(new ApiKeyServiceClientCredentials(azureKey), new DelegatingHandler[] { });
+			faceClient.Endpoint = azureEndpoint;
 		}
 
 		private void VideoSource_NewFrame(object sender, NewFrameEventArgs args)
 		{
 			if (args.Frame.Clone() is Bitmap bitmap)
 			{
+				lock (latestImage)
+				{
+					latestImage = bitmap;
+				}
 				this.Dispatcher.Invoke(() =>
 				{
 					BitmapImage source = new BitmapImage();
 					using (Stream stream = new MemoryStream())
 					{
-						bitmap.Save(stream, ImageFormat.Png);
+						bitmap.Save(stream, ImageFormat.Bmp);
 						stream.Position = 0;
 						source.BeginInit();
 						source.CacheOption = BitmapCacheOption.OnLoad;
@@ -55,6 +74,79 @@ namespace FaceFollower
 					image.Source = source;
 				});
 			}
+		}
+
+		private void image_SizeChanged(object sender, SizeChangedEventArgs e)
+		{
+			canvas.Width = e.NewSize.Width;
+		}
+
+		private void StartStop_Click(object sender, RoutedEventArgs e)
+		{
+			if (faceDetectionRunning)
+			{
+				faceDetectionRunning = false;
+				faceRect.Visibility = Visibility.Hidden;
+				SetStatus("Stopped");
+			}
+			else
+			{
+				faceDetectionRunning = true;
+				Task.Run(Timer_Tick);
+				SetStatus("Running...");
+			}
+		}
+
+		private async void Timer_Tick()
+		{
+			while (faceDetectionRunning)
+			{
+				try
+				{
+					IList<DetectedFace> faces;
+					using (Stream stream = new MemoryStream())
+					{
+						lock (latestImage)
+						{
+							latestImage.Save(stream, ImageFormat.Bmp);
+						}
+						stream.Position = 0;
+						faces = await faceClient.Face.DetectWithStreamAsync(stream);
+					}
+
+					if (faceDetectionRunning)
+					{
+						this.Dispatcher.Invoke(() =>
+						{
+							if (faces.Count > 0 && faces[0] is DetectedFace face)
+							{
+								faceRect.Visibility = Visibility.Visible;
+								faceRect.Width = face.FaceRectangle.Width / (double)latestImage.Width * image.ActualWidth;
+								faceRect.Height = face.FaceRectangle.Height / (double)latestImage.Height * image.ActualHeight;
+								Canvas.SetLeft(faceRect, face.FaceRectangle.Left / (double)latestImage.Width * image.ActualWidth);
+								Canvas.SetTop(faceRect, face.FaceRectangle.Top / (double)latestImage.Height * image.ActualHeight);
+								SetStatus($"Running: Found face [{face.FaceRectangle.Left}, {face.FaceRectangle.Top}, {face.FaceRectangle.Width}, {face.FaceRectangle.Height}]");
+							}
+							else
+							{
+								SetStatus("Running: No face found");
+							}
+						});
+					}
+				}
+				// Catch and display Face API errors.
+				catch (APIErrorException ex)
+				{
+					MessageBox.Show(ex.Message, "API Error");
+				}
+
+				await Task.Delay(3000);
+			}
+		}
+
+		private void SetStatus(string msg)
+		{
+			status.Text = $"Status {DateTime.Now:hh:mm:ss}: {msg}";
 		}
 
 		protected override void OnClosed(EventArgs e)
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # FaceFollower
+
+Before running, set the enviroment variables:  
+`"AZURE_FACE_KEY" = "<AZURE_FACE_KEY>"`  
+`"AZURE_FACE_ENDPOINT" = "https://<location>.api.cognitive.microsoft.com"` (no subdirectories or trailing slashes)
